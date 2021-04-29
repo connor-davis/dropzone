f7e1d0780da31783992ed01d2e1399845bf91c8e
Connor Davis <19422@kloofhigh.co.za>
2021-04-29 22:58:25 +0200

    change tempFiles to temp

--- electron/electron.js
+++ electron/electron.js
@@ -1,10 +1,10 @@
 let path = require('path');
 let { app, Menu, ipcMain } = require('electron');
 let {
-    createWindow,
-    defineWindow,
-    getWindow,
-    closeAllWindows,
+  createWindow,
+  defineWindow,
+  getWindow,
+  closeAllWindows,
 } = require('./electronWindows');
 let { autoUpdater } = require('electron-updater');
 
@@ -21,38 +21,38 @@ let dropzone;
 let { ProgId, Regedit } = require('electron-regedit');
 
 new ProgId({
-    description: 'DropZone Droplet',
-    icon: 'droplet.ico',
-    extensions: ['droplet'],
+  description: 'DropZone Droplet',
+  icon: 'droplet.ico',
+  extensions: ['droplet'],
 });
 
 Regedit.installAll();
 
-if (!fs.existsSync(path.join(process.cwd(), 'tempFiles')))
-    fs.mkdirSync(path.join(process.cwd(), 'tempFiles'));
+if (!fs.existsSync(path.join(process.cwd(), 'temp')))
+  fs.mkdirSync(path.join(process.cwd(), 'temp'));
 
 /**
  * Creates a window for the main application.
  * @returns {Window}
  */
 function createMainWindow() {
-    let windowOptions = {
-        width: 1280,
-        minWidth: 480,
-        height: 720,
-        minHeight: 480,
-        show: false,
-        center: true,
-        autoHideMenuBar: true,
-        webPreferences: {
-            nodeIntegration: true,
-            nodeIntegrationInWorker: true,
-            contextIsolation: false,
-            preload: path.join(__dirname, 'preload.js'),
-        },
-        title: app.name,
-    };
-    return createWindow(MAIN_WINDOW_ID, windowOptions);
+  let windowOptions = {
+    width: 1280,
+    minWidth: 480,
+    height: 720,
+    minHeight: 480,
+    show: false,
+    center: true,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
+      contextIsolation: false,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    title: app.name,
+  };
+  return createWindow(MAIN_WINDOW_ID, windowOptions);
 }
 
 /**
@@ -61,69 +61,67 @@ function createMainWindow() {
  * @returns {Electron.BrowserWindow}
  */
 function createSplashWindow() {
-    let windowOptions = {
-        width: 400,
-        height: 200,
-        resizable: false,
-        autoHideMenuBar: true,
-        frame: false,
-        show: true,
-        center: true,
-        title: app.name,
-    };
-    let window = defineWindow('splash', windowOptions);
+  let windowOptions = {
+    width: 400,
+    height: 200,
+    resizable: false,
+    autoHideMenuBar: true,
+    frame: false,
+    show: true,
+    center: true,
+    title: app.name,
+  };
+  let window = defineWindow('splash', windowOptions);
 
-    if (IS_DEVELOPMENT) {
-        window.loadURL('http://localhost:3000/splash.html').then();
-    } else {
-        window.loadURL(`file://${path.join(__dirname, '/splash.html')}`).then();
-    }
+  if (IS_DEVELOPMENT) {
+    window.loadURL('http://localhost:3000/splash.html').then();
+  } else {
+    window.loadURL(`file://${path.join(__dirname, '/splash.html')}`).then();
+  }
 
-    return window;
+  return window;
 }
 
 // attach process logger
 
 process.on('uncaughtException', (err) => {
-    console.error(err);
-    closeAllWindows();
+  console.error(err);
+  closeAllWindows();
 });
 
 // build menu
 
 let menuTemplate = [
-    {
-        label: 'Window',
-        role: 'window',
-        submenu: [
-            {
-                label: 'Minimize',
-                accelerator: 'CmdOrCtrl+M',
-                role: 'minimize',
-            },
-            {
-                label: 'Reload',
-                accelerator: 'CmdOrCtrl+R',
-                click: function (item, focusedWindow) {
-                    if (focusedWindow) {
-                        focusedWindow.reload();
-                    }
-                },
-            },
-            {
-                label: 'Toggle Developer Tools',
-                accelerator:
-                    process.platform === 'darwin'
-                        ? 'Alt+Command+I'
-                        : 'Ctrl+Shift+I',
-                click: function (item, focusedWindow) {
-                    if (focusedWindow) {
-                        focusedWindow.webContents.toggleDevTools();
-                    }
-                },
-            },
-        ],
-    },
+  {
+    label: 'Window',
+    role: 'window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize',
+      },
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function (item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.reload();
+          }
+        },
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator:
+          process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click: function (item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.webContents.toggleDevTools();
+          }
+        },
+      },
+    ],
+  },
 ];
 let menu = Menu.buildFromTemplate(menuTemplate);
 Menu.setApplicationMenu(menu);
@@ -133,41 +131,41 @@ Menu.setApplicationMenu(menu);
 app.requestSingleInstanceLock();
 
 app.on('second-instance', () => {
-    let window = getWindow(MAIN_WINDOW_ID);
-    if (window) {
-        if (window.isMinimized()) {
-            window.restore();
-        }
-        window.focus();
+  let window = getWindow(MAIN_WINDOW_ID);
+  if (window) {
+    if (window.isMinimized()) {
+      window.restore();
     }
+    window.focus();
+  }
 });
 
 // quit application when all windows are closed
 app.on('window-all-closed', () => {
-    // on macOS it is common for applications to stay open until the user explicitly quits
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  // on macOS it is common for applications to stay open until the user explicitly quits
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    // on macOS it is common to re-create a window even after all windows have been closed
-    let window = getWindow(MAIN_WINDOW_ID);
-    if (window === null) {
-        createMainWindow();
-    }
+  // on macOS it is common to re-create a window even after all windows have been closed
+  let window = getWindow(MAIN_WINDOW_ID);
+  if (window === null) {
+    createMainWindow();
+  }
 });
 
 // create main BrowserWindow with a splash screen when electron is ready
 app.on('ready', () => {
-    let splashWindow = createSplashWindow();
-    let mainWindow = createMainWindow();
-    mainWindow.once('ready-to-show', () => {
-        setTimeout(() => {
-            splashWindow.close();
-            mainWindow.show();
-        }, 300);
-    });
+  let splashWindow = createSplashWindow();
+  let mainWindow = createMainWindow();
+  mainWindow.once('ready-to-show', () => {
+    setTimeout(() => {
+      splashWindow.close();
+      mainWindow.show();
+    }, 300);
+  });
 });
 
 /**
@@ -177,7 +175,7 @@ app.on('ready', () => {
 autoUpdater.on('checking-for-update', () => {});
 
 autoUpdater.on('update-available', (info) => {
-    autoUpdater.downloadUpdate();
+  autoUpdater.downloadUpdate();
 });
 
 autoUpdater.on('update-not-available', (info) => {});
@@ -187,9 +185,9 @@ autoUpdater.on('error', (err) => {});
 autoUpdater.on('download-progress', (progressObj) => {});
 
 autoUpdater.on('update-downloaded', (info) => {
-    setTimeout(() => {
-        autoUpdater.quitAndInstall();
-    }, 500);
+  setTimeout(() => {
+    autoUpdater.quitAndInstall();
+  }, 500);
 });
 
 /**
@@ -197,108 +195,108 @@ autoUpdater.on('update-downloaded', (info) => {
  */
 
 ipcMain.on('connect', (event, channel) => {
-    try {
-        if (dropzone) {
-            dropzone.destroy();
-            dropzone = null;
-        }
-
-        dropzone = new DropZone({
-            channel,
-            swarm: HyperSwarm(),
-        });
-
-        dropzone.on('packet', (packet) => event.sender.send('packet', packet));
-
-        dropzone._channel.on('disconnected', () => {
-            event.sender.send('disconnected');
-        });
-
-        dropzone.on('peer', (peer) => {
-            event.sender.send('packet', {
-                type: 'peer',
-                peerIdentity: peer.identity,
-            });
-        });
-
-        dropzone.on('channel', (_channel) => {
-            event.sender.send('packet', { type: 'joined', channel });
-        });
-    } catch (error) {
-        event.sender.send('packet', {
-            type: 'error',
-            error,
-        });
+  try {
+    if (dropzone) {
+      dropzone.destroy();
+      dropzone = null;
     }
+
+    dropzone = new DropZone({
+      channel,
+      swarm: HyperSwarm(),
+    });
+
+    dropzone.on('packet', (packet) => event.sender.send('packet', packet));
+
+    dropzone._channel.on('disconnected', () => {
+      event.sender.send('disconnected');
+    });
+
+    dropzone.on('peer', (peer) => {
+      event.sender.send('packet', {
+        type: 'peer',
+        peerIdentity: peer.identity,
+      });
+    });
+
+    dropzone.on('channel', (_channel) => {
+      event.sender.send('packet', { type: 'joined', channel });
+    });
+  } catch (error) {
+    event.sender.send('packet', {
+      type: 'error',
+      error,
+    });
+  }
 });
 
 ipcMain.on('disconnect', (event) => {
-    try {
-        if (dropzone) {
-            dropzone.destroy();
-            dropzone = null;
-        }
-    } catch (error) {
-        event.sender.send('packet', {
-            type: 'error',
-            error,
-        });
+  try {
+    if (dropzone) {
+      dropzone.destroy();
+      dropzone = null;
     }
+  } catch (error) {
+    event.sender.send('packet', {
+      type: 'error',
+      error,
+    });
+  }
 });
 
 ipcMain.on('upload', async (event, packet) => {
-    try {
-        await dropzone.requestFileTransfer(packet);
-    } catch (error) {
-        event.sender.send('packet', {
-            type: 'error',
-            error,
-        });
-    }
+  try {
+    await dropzone.requestFileTransfer(packet);
+  } catch (error) {
+    event.sender.send('packet', {
+      type: 'error',
+      error,
+    });
+  }
 });
 
 ipcMain.on('acceptTransfer', async (event, request) => {
-    try {
-        await dropzone.acceptFileTransfer(request);
-        await dropzone.receiveFile(request);
-    } catch (error) {
-        event.sender.send('packet', {
-            type: 'error',
-            error,
-        });
-    }
+  try {
+    await dropzone.acceptFileTransfer(request);
+    await dropzone.receiveFile(request);
+  } catch (error) {
+    event.sender.send('packet', {
+      type: 'error',
+      error,
+    });
+  }
 });
 
 ipcMain.on('rejectTransfer', async (event, request) => {
-    try {
-        await dropzone.rejectFileTransfer(request);
-    } catch (error) {
-        event.sender.send('packet', {
-            type: 'error',
-            error,
-        });
-    }
+  try {
+    await dropzone.rejectFileTransfer(request);
+  } catch (error) {
+    event.sender.send('packet', {
+      type: 'error',
+      error,
+    });
+  }
 });
 
 ipcMain.on('delete', (event, id) => {
-    try {
-        fs.unlinkSync(path.join(process.cwd(), 'tempFiles', id + '.droplet'));
-        event.sender.send('deleted', id);
-    } catch (error) {
-        event.sender.send('packet', {
-            type: 'error',
-            error,
-        });
-    }
+  try {
+    fs.unlinkSync(path.join(process.cwd(), 'temp', id + '.droplet'));
+    event.sender.send('deleted', id);
+  } catch (error) {
+    event.sender.send('packet', {
+      type: 'error',
+      error,
+    });
+  }
 });
 
 ipcMain.on('message', (event, packet) => {
-    try {
-        dropzone._channel.packet(packet);
-    } catch (error) {
-        event.sender.send('packet', {
-            type: 'error',
-            error,
-        });
-    }
+  try {
+    dropzone._channel.packet(packet);
+  } catch (error) {
+    event.sender.send('packet', {
+      type: 'error',
+      error,
+    });
+  }
 });
--- electron/utils/connector/ConnectorPeer.js
+++ electron/utils/connector/ConnectorPeer.js
@@ -7,206 +7,202 @@ let fs = require('fs');
 let path = require('path');
 
 class ConnectorPeer extends EventEmitter {
-    constructor(connection, information) {
-        super();
+  constructor(connection, information) {
+    super();
 
-        let peer = information.peer;
+    let peer = information.peer;
 
-        this.identity = uuid.v4();
-        this._connection = connection;
-        this._incoming = ndjson.parse();
-        this._outgoing = ndjson.stringify();
+    this.identity = uuid.v4();
+    this._connection = connection;
+    this._incoming = ndjson.parse();
+    this._outgoing = ndjson.stringify();
 
-        connection.pipe(this._incoming);
-        this._outgoing.pipe(connection);
+    connection.pipe(this._incoming);
+    this._outgoing.pipe(connection);
 
-        this.fileData = {};
+    this.fileData = {};
 
-        this._incoming.on('data', async (data) => {
-            this.emit('data', data);
+    this._incoming.on('data', async (data) => {
+      this.emit('data', data);
 
-            let { type } = data;
+      let { type } = data;
 
-            switch (type) {
-                case 'start':
-                    this.emit('packet', {
-                        type: 'info',
-                        message: 'Download started for ' + data.fileName,
-                    });
-
-                    this.fileData = {
-                        ...data,
-                        filePath: path.join(
-                            process.cwd(),
-                            'tempFiles',
-                            data.fileIdentity + '.droplet'
-                        ),
-                    };
-
-                    this.emit('packet', {
-                        type: 'debug',
-                        fileData: this.fileData,
-                    });
-
-                    if (!fs.existsSync(this.fileData.filePath))
-                        fs.writeFileSync(this.fileData.filePath, '');
-
-                    this.emit('packet', {
-                        ...this.fileData,
-                        type: 'start-download',
-                    });
-
-                    break;
-
-                case 'progress':
-                    this.emit('packet', { ...data, type: 'progress-download' });
-
-                    break;
-
-                case 'Buffer':
-                    fs.appendFileSync(
-                        this.fileData.filePath,
-                        Buffer.from(data.data)
-                    );
-
-                    break;
-                case 'finish':
-                    this.emit('packet', {
-                        type: 'info',
-                        message:
-                            'Download finished for ' + this.fileData.fileName,
-                    });
-
-                    this.emit('packet', {
-                        ...this.fileData,
-                        type: 'finish-download',
-                    });
-
-                    this.destroy();
-
-                    break;
-
-                default:
-                    break;
-            }
-
-            this.emit(type, data);
-        });
-
-        this._connection.on('error', (error) => {
-            this.emit('error', error);
-        });
-
-        this._connection.on('close', () => {
-            this.emit('disconnected');
-        });
-
-        if (peer && peer.topic) {
-            let peerChannel = peer.topic.toString('hex');
-
-            this.send({
-                type: 'handshake',
-                peerChannel,
-            });
-
-            setTimeout(() => {
-                this.emit('channel', peerChannel);
-            }, 0);
-        } else {
-            this.once('handshake', ({ peerChannel }) => {
-                this.send({
-                    type: 'handshake',
-                    peerChannel,
-                });
-                this.emit('channel', peerChannel);
-            });
-        }
-    }
-
-    send(data) {
-        this._outgoing.write(data);
-    }
-
-    file(filePath, { fileIdentity, fileName, fileType, fileSize }) {
-        this._outgoing.write({
-            type: 'start',
-            fileIdentity,
-            fileName,
-            fileType,
-            fileSize,
-        });
-
-        this.emit('packet', {
+      switch (type) {
+        case 'start':
+          this.emit('packet', {
             type: 'info',
-            message: 'Upload started for ' + fileName,
-        });
+            message: 'Download started for ' + data.fileName,
+          });
 
-        this.emit('packet', {
-            type: 'start-upload',
+          this.fileData = {
+            ...data,
+            filePath: path.join(
+              process.cwd(),
+              'temp',
+              data.fileIdentity + '.droplet'
+            ),
+          };
+
+          this.emit('packet', {
+            type: 'debug',
+            fileData: this.fileData,
+          });
+
+          if (!fs.existsSync(this.fileData.filePath))
+            fs.writeFileSync(this.fileData.filePath, '');
+
+          this.emit('packet', {
+            ...this.fileData,
+            type: 'start-download',
+          });
+
+          break;
+
+        case 'progress':
+          this.emit('packet', { ...data, type: 'progress-download' });
+
+          break;
+
+        case 'Buffer':
+          fs.appendFileSync(this.fileData.filePath, Buffer.from(data.data));
+
+          break;
+        case 'finish':
+          this.emit('packet', {
+            type: 'info',
+            message: 'Download finished for ' + this.fileData.fileName,
+          });
+
+          this.emit('packet', {
+            ...this.fileData,
+            type: 'finish-download',
+          });
+
+          this.destroy();
+
+          break;
+
+        default:
+          break;
+      }
+
+      this.emit(type, data);
+    });
+
+    this._connection.on('error', (error) => {
+      this.emit('error', error);
+    });
+
+    this._connection.on('close', () => {
+      this.emit('disconnected');
+    });
+
+    if (peer && peer.topic) {
+      let peerChannel = peer.topic.toString('hex');
+
+      this.send({
+        type: 'handshake',
+        peerChannel,
+      });
+
+      setTimeout(() => {
+        this.emit('channel', peerChannel);
+      }, 0);
+    } else {
+      this.once('handshake', ({ peerChannel }) => {
+        this.send({
+          type: 'handshake',
+          peerChannel,
+        });
+        this.emit('channel', peerChannel);
+      });
+    }
+  }
+
+  send(data) {
+    this._outgoing.write(data);
+  }
+
+  file(filePath, { fileIdentity, fileName, fileType, fileSize }) {
+    this._outgoing.write({
+      type: 'start',
+      fileIdentity,
+      fileName,
+      fileType,
+      fileSize,
+    });
+
+    this.emit('packet', {
+      type: 'info',
+      message: 'Upload started for ' + fileName,
+    });
+
+    this.emit('packet', {
+      type: 'start-upload',
+      fileIdentity,
+      fileName,
+      fileType,
+      fileSize,
+    });
+
+    let progressStream = progress({
+      length: fileSize,
+      time: 1000,
+    });
+
+    progressStream.on('progress', (progress) => {
+      this._outgoing.write({
+        type: 'progress',
+        fileIdentity,
+        percentage: progress.percentage,
+        speed: progress.speed,
+        eta: progress.eta,
+      });
+
+      this.emit('packet', {
+        type: 'progress-upload',
+        fileIdentity,
+        percentage: progress.percentage,
+        speed: progress.speed,
+        eta: progress.eta,
+      });
+    });
+
+    setTimeout(() => {
+      let transfer = fs
+        .createReadStream(filePath)
+        .pipe(progressStream)
+        .on('end', () => {
+          this._outgoing.write({
+            type: 'finish',
             fileIdentity,
             fileName,
             fileType,
             fileSize,
-        });
+          });
 
-        let progressStream = progress({
-            length: fileSize,
-            time: 1000,
-        });
+          this.emit('packet', {
+            type: 'info',
+            message: 'Upload finished for ' + fileName,
+          });
 
-        progressStream.on('progress', (progress) => {
-            this._outgoing.write({
-                type: 'progress',
-                fileIdentity,
-                percentage: progress.percentage,
-                speed: progress.speed,
-                eta: progress.eta,
-            });
+          this.emit('packet', {
+            type: 'finish-upload',
+            fileIdentity,
+            fileName,
+            fileType,
+            fileSize,
+          });
 
-            this.emit('packet', {
-                type: 'progress-upload',
-                fileIdentity,
-                percentage: progress.percentage,
-                speed: progress.speed,
-                eta: progress.eta,
-            });
-        });
+          this.destroy();
+        })
+        .pipe(this._outgoing);
+    }, 500);
+  }
 
-        setTimeout(() => {
-            let transfer = fs
-                .createReadStream(filePath)
-                .pipe(progressStream)
-                .on('end', () => {
-                    this._outgoing.write({
-                        type: 'finish',
-                        fileIdentity,
-                        fileName,
-                        fileType,
-                        fileSize,
-                    });
-
-                    this.emit('packet', {
-                        type: 'info',
-                        message: 'Upload finished for ' + fileName,
-                    });
-
-                    this.emit('packet', {
-                        type: 'finish-upload',
-                        fileIdentity,
-                        fileName,
-                        fileType,
-                        fileSize,
-                    });
-
-                    this.destroy();
-                })
-                .pipe(this._outgoing);
-        }, 500);
-    }
-
-    destroy() {
-        this._connection.end();
-    }
+  destroy() {
+    this._connection.end();
+  }
 }
 
 module.exports = ConnectorPeer;
