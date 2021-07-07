44a3357e7e4f86e10843ee151e1c21e3a094af17
Connor Davis <19422@kloofhigh.co.za>
2021-07-07 22:59:49 +0200

    update requests icon

--- src/pages/friendList.js
+++ src/pages/friendList.js
@@ -50,7 +50,7 @@ let FriendListPage = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              d="M8 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-2m-4-1v8m0 0l3-3m-3 3L9 8m-5 5h2.586a1 1 0 01.707.293l2.414 2.414a1 1 0 00.707.293h3.172a1 1 0 00.707-.293l2.414-2.414a1 1 0 01.707-.293H20"
             />
           </svg>
         </div>
