c06504addd2d25d53ede3c00408699a7428261d3
Connor Davis <19422@kloofhigh.co.za>
2021-07-09 00:05:26 +0200

    trying to fix this damn error

--- electron/electron.js
+++ electron/electron.js
@@ -220,14 +220,22 @@ ipcMain.on('initiateNode', async (event, packet0) => {
       io.on('connection', (socket) => {});
 
       server.listen();
+
+      fs.writeFileSync(
+        `${process.cwd()}/userData/publicKey.droplet`,
+        crypto
+          .keyPair(crypto.data(Buffer.from(packet0.username + '.dropZoneNode')))
+          .publicKey.toString('hex'),
+        { encoding: 'utf8' }
+      );
     }
   });
 });
 
 ipcMain.on('copyPublicKey', (event, packet0) => {
-  let publicKey = crypto
-    .keyPair(crypto.data(Buffer.from(packet0.username + '.dropZoneNode')))
-    .publicKey.toString('hex');
+  let publicKey = fs.readFileSync(
+    `${process.cwd()}/userData/publicKey.droplet`
+  );
 
   clipboard.writeText(publicKey, 'clipboard');
 
